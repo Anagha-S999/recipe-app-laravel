1c18d004413078360b31614465c164009e27b245
Anagha-S999 <mca507@rajagiri.edu>
2021-03-19 20:21:40 +0530

    themes added

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+@extends("theme")
+
+@section("contents")
+
+<div class="container">
+          <div class="row">
+              <div class="col col-12 col-sm-6 col-lg-6">
+                  <img height="500px" src="https://t4.ftcdn.net/jpg/03/31/50/87/360_F_331508755_eDTtcgYlNmjxC7MZIsRgXGsARgkigSaB.jpg" alt="">
+              </div>
+              <div class="col col-12 col-sm-6 col-lg-6">
+                  <h2><center>OUR STORY</center></h2>
+                  <p><i>Tasty Kitchen is a trusted site for home cooks with more recipies.. We have a team of experienced chefs. We help you to cook delicious food with less stress and more joy. We offer recipies for home makers..</i></p>
+                  <p><i><span style="font-family: Verdana, Geneva, Tahoma, sans-serif;">"We bring you the best and soul touched recipies"</span></i></p>
+              </div>
+          </div>
+      </div>
+
+@endsection
